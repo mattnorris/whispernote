eecb8d6135b01fb9e3c3e3bdb26b5d796ca3c35f
Matt Norris <m@mattnorris.me>
2013-03-24 23:37:06 -0400

    Added Google Analytics tracking and Fork Me ribbon.

--- scripts/watch.bat
+++ scripts/watch.bat
@@ -1,6 +1,6 @@
 REM Run Jekyll and watch-less simultaneously while working on this project.
 REM http://superuser.com/questions/345602
 cd ..
-start /B jekyll
-start /B watch-less -c -d assets/css
+start jekyll
+start watch-less -c -d assets/css
 cd scripts
